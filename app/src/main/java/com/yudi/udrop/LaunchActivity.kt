8e8ee2deea8a65c2f839c55894d80b85573a5348
Beijia <beijia.dai@sap.com>
2021-10-08 22:22:00 +0800

    Some file names are standardized

--- app/src/main/java/com/yudi/udrop/LaunchActivity.kt
+++ app/src/main/java/com/yudi/udrop/LaunchActivity.kt
@@ -1,6 +1,5 @@
 package com.yudi.udrop
 
-import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,7 +8,7 @@ import android.widget.Button
 class LaunchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_launch)
         //实现点击页面跳转
         findViewById<Button>(R.id.login_btn).setOnClickListener {
             startActivity(Intent(this,LoginActivity::class.java))

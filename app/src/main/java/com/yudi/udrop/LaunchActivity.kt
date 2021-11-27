e13b8f6c4e941ad7df4ab08b51fb5957377173b5
Beijia <beijia.dai@sap.com>
2021-11-27 23:16:53 +0800

    Tiny fix.

--- app/src/main/java/com/yudi/udrop/LaunchActivity.kt
+++ app/src/main/java/com/yudi/udrop/LaunchActivity.kt
@@ -1,20 +1,20 @@
 package com.yudi.udrop
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class LaunchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch)
         //实现点击页面跳转
-        findViewById<Button>(R.id.launch_login).setOnClickListener {
-            startActivity(Intent(this,LoginActivity::class.java))
+        findViewById<TextView>(R.id.launch_login).setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
         }
-        findViewById<Button>(R.id.launch_register).setOnClickListener {
-            startActivity(Intent(this,RegisterActivity::class.java))
+        findViewById<TextView>(R.id.launch_register).setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
         }
     }
 }

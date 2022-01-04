f1dd0daf46f91576ca0741944ccbf1201d0eb96b
Jia-algorithm <774739481@qq.com>
2022-01-04 21:48:19 +0800

    Update udrop page

--- app/src/main/java/com/yudi/udrop/adapter/MessageAdapter.kt
+++ app/src/main/java/com/yudi/udrop/adapter/MessageAdapter.kt
@@ -1,7 +1,5 @@
 package com.yudi.udrop.adapter
 
-import android.content.Context
-import android.text.format.DateFormat
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
@@ -9,21 +7,16 @@ import androidx.recyclerview.widget.RecyclerView
 import com.yudi.udrop.R
 import com.yudi.udrop.databinding.UdropMessageItemBinding
 import com.yudi.udrop.model.data.MessageModel
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 
-class MessageAdapter(val context: Context) : RecyclerView.Adapter<MessageAdapter.ViewHolder>() {
+class MessageAdapter : RecyclerView.Adapter<MessageAdapter.ViewHolder>() {
     inner class ViewHolder(val binding: UdropMessageItemBinding) :
         RecyclerView.ViewHolder(binding.root)
 
-    lateinit var messageList : ArrayList<MessageModel>
+    private var messageList: ArrayList<MessageModel> = arrayListOf()
 
-    private fun getData() {
-        var newMessage : ArrayList<MessageModel> = ArrayList()
-        val TimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss")
-        newMessage.add(MessageModel(false, "你好，我是语滴。", "${TimeFormatter.format(LocalDateTime.now())}"))
-        newMessage.add(MessageModel(false, "你可以和我聊天，也可以输入“背书”，开始背书之旅哦~", "${TimeFormatter.format(LocalDateTime.now())}"))
-        messageList = newMessage
+    fun updateContent(position: Int, message: MessageModel) {
+        messageList.add(message)
+        notifyItemChanged(position)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -37,11 +30,10 @@ class MessageAdapter(val context: Context) : RecyclerView.Adapter<MessageAdapter
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        getData()
         holder.binding.model = messageList[position]
 //        val TimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss")
 //        holder.binding.model = MessageModel(false,"你好，我是语滴！","${TimeFormatter.format(LocalDateTime.now())}")
     }
 
-    override fun getItemCount(): Int = 2
+    override fun getItemCount(): Int = messageList.size
 }
--- app/src/main/java/com/yudi/udrop/fragment/UdropFragment.kt
+++ app/src/main/java/com/yudi/udrop/fragment/UdropFragment.kt
@@ -4,37 +4,51 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.Nullable
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.yudi.udrop.R
 import com.yudi.udrop.adapter.MessageAdapter
-import com.yudi.udrop.databinding.FragmentUdropBinding
-import com.yudi.udrop.model.data.ToolbarModel
+import com.yudi.udrop.model.data.MessageModel
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class UdropFragment : Fragment() {
-    lateinit var binding: FragmentUdropBinding
 
-    @Nullable
+    private val adapter: MessageAdapter = MessageAdapter()
+
     override fun onCreateView(
         inflater: LayoutInflater,
-        @Nullable container: ViewGroup?,
-        @Nullable savedInstanceState: Bundle?
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
-        binding = FragmentUdropBinding.inflate(inflater)
-        return binding.root
+        return inflater.inflate(R.layout.fragment_udrop, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.model = ToolbarModel("语滴助手")
+        Glide.with(view)
+            .load(R.drawable.siri)
+            .into(view.findViewById(R.id.udrop_microphone_gif))
         setupRecyclerView(view)
+        val TimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss")
+        adapter.updateContent(
+            0,
+            MessageModel(false, "你好，我是语滴。", "${TimeFormatter.format(LocalDateTime.now())}")
+        )
+        adapter.updateContent(
+            1,
+            MessageModel(
+                false,
+                "你可以和我聊天，也可以输入“背书”，开始背书之旅哦~",
+                "${TimeFormatter.format(LocalDateTime.now())}"
+            )
+        )
     }
 
     private fun setupRecyclerView(view: View) {
-        val recyclerView = view.findViewById<RecyclerView>(R.id.fragment_udrop_content)
-        val adapter = context?.let { MessageAdapter(it) }
+        val recyclerView = view.findViewById<RecyclerView>(R.id.udrop_content)
         recyclerView.adapter = adapter
         val layoutManager = LinearLayoutManager(context)
         layoutManager.orientation = RecyclerView.VERTICAL

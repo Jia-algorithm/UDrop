d429d0c5159f184c4f51f2cf92cdb36646aadc54
beijia.dai@sap.com <beijia.dai@sap.com>
2022-02-21 16:02:40 +0800

    fix

--- app/src/main/java/com/yudi/udrop/model/data/ScheduleModel.kt
+++ app/src/main/java/com/yudi/udrop/model/data/ScheduleModel.kt
@@ -1,6 +1,10 @@
 package com.yudi.udrop.model.data
 
-data class ScheduleModel(val completedNumber: Int, val totalNumber: Int) {
+data class ScheduleModel(val _completedNumber: Int, val _totalNumber: Int) {
+    val completedNumber: String
+        get() = _completedNumber.toString()
+    val totalNumber: String
+        get() = _totalNumber.toString()
     val process: String
         get() = "已学 $completedNumber/$totalNumber"
 }
